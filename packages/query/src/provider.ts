1699f119821da3e3325c022beb5a101f78e7a315
Irakli Gozalishvili <contact@gozala.io>
2022-04-05 12:48:17 -0700

    chore: refactor backend interface

---
+++ packages/query/src/provider.ts
@@ -0,0 +1,93 @@
+import type { Link, DID } from '@ipld/dag-ucan'
+
+export interface Context<DID, Link, Token> {
+  proofs: ProofsProvider<Token>
+  store: StoreProvider<DID, Link, Token>
+}
+
+export interface ProofsProvider<T> {
+  /**
+   * Service MAY need to lookup a proof by CID that was previously stored. In
+   * such case it will call this to do a lookup.
+   */
+  get(
+    cid: Link<T>
+  ): Result<T, ProofNotFoundError | RevokedError | InvalidProofError>
+
+  /**
+   * Checks status of the given proof by it's CID. Just like get execept when
+   * actual value is irrelevant.
+   */
+  status(
+    cid: Link<T>
+  ): Result<void, ProofNotFoundError | RevokedError | InvalidProofError>
+
+  /**
+   * Saves valid proof and associated token. Calling `get` after this should return `T`
+   * unless it has been rovked. It may error with `RevokedError` if it had
+   * been revoked concurrently by other actor.
+   *
+   */
+  validate(cid: Link<T>, token: T): Result<undefined, RevokedError>
+
+  /**
+   * Saves invalid proof so that we can avoid validating it again.
+   */
+  invalidate(
+    cid: Link<T>,
+    token: T,
+    reason: InvalidProofError
+  ): Result<undefined, ProofNotFoundError>
+}
+
+export interface StoreProvider<DID, Link, Proof> {
+  /**
+   * Service will call this once it verified the UCAN to associate link with a
+   * given DID. Service is unaware if given `DID` is associated with some account
+   * or not, if it is not `StoreProvider` MUST return `UnauthorizedDIDError`.
+   */
+  add(
+    group: DID,
+    link: Link,
+    proof: Proof
+  ): Result<AddStatus, UnauthorizedDIDError | QuotaViolationError>
+  remove(
+    group: DID,
+    link: Link,
+    ucan: Proof
+  ): Result<undefined, UnauthorizedDIDError | DoesNotHasError>
+}
+
+export interface AddStatus {
+  /**
+   * Should be `ok` if we already have car and we don't need to perform upload.
+   * Otherwise should be `pending`.
+   */
+  status: 'ok' | 'pending'
+}
+
+export interface ProofNotFoundError extends Error {
+  cid: Link
+}
+
+export interface QuotaViolationError extends Error {
+  group: DID
+  link: Link
+}
+
+/**
+ * Token has been rovked
+ */
+export interface RevokedError extends Error {
+  cid: Link
+}
+
+export interface InvalidProofError extends Error {
+  cid: Link
+}
+
+export interface UnauthorizedDIDError extends Error {}
+
+export interface DoesNotHasError extends Error {}
+
+export type Result<T, X> = { ok: true; value: T } | { ok: false; error: X }
